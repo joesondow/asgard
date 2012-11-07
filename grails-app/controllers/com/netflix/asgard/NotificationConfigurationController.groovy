e552697b383568c8a4623818768cd10fb56f5897
Joe Sondow <jsondow@netflix.com>
2012-11-06 17:02:04 -0800

    ASGARD-1000 Notification config list action

---
+++ grails-app/controllers/com/netflix/asgard/NotificationConfigurationController.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.asgard
+
+import com.amazonaws.services.autoscaling.model.NotificationConfiguration
+import grails.converters.JSON
+import grails.converters.XML
+
+class NotificationConfigurationController {
+
+    def awsAutoScalingService
+
+    def allowedMethods = [save: 'POST', update: 'POST', delete: 'POST']
+
+    def index = { redirect(action: 'list', params: params) }
+
+    def list = {
+        UserContext userContext = UserContext.of(request)
+        List<NotificationConfiguration> notificationConfigurations =
+                awsAutoScalingService.getAllNotificationConfigurations(userContext).sort { it.autoScalingGroupName }
+        withFormat {
+            html { [notificationConfigurations: notificationConfigurations] }
+            xml { new XML(notificationConfigurations).render(response) }
+            json { new JSON(notificationConfigurations).render(response) }
+        }
+    }
+}
